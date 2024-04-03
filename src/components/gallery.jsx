442b9231e00a60d181d001266fdc78701d714515
dnl-kg <dnlgakpetor@gmail.com>
2024-04-03 12:44:28 -0400

    Media Page updated

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -6,23 +6,22 @@ export const Gallery = (props) => {
     <div id="portfolio" className="text-center">
       <div className="container">
         <div className="section-title">
-          <h2>Gallery</h2>
+          <h2>Media</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+            Significant media from Jesus-city Events, Articles and Publications
           </p>
         </div>
         <div className="row">
-          <div className="portfolio-items">
+          <div className="portfolio-items w-full grid grid-cols-1 md:grid-cols-3">
             {props.data
               ? props.data.map((d, i) => (
                   <div
                     key={`${d.title}-${i}`}
-                    className="col-sm-6 col-md-4 col-lg-4"
+                    className="flex justify-center mb-4"
                   >
                     <Image
                       title={d.title}
-                      largeImage={d.largeImage}
+                      link={d.link}
                       smallImage={d.smallImage}
                     />
                   </div>
--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-export const Image = ({ title, largeImage, smallImage }) => {
+export const Image = ({ title, link, smallImage}) => {
   return (
     <div className="portfolio-item">
       <div className="hover-bg">
         {" "}
-        <a href={largeImage} title={title} data-lightbox-gallery="gallery1">
+        <a href={link} title={title} data-lightbox-gallery="gallery1">
           <div className="hover-text">
             <h4>{title}</h4>
           </div>
-          <img src={smallImage} className="img-responsive" alt={title} />{" "}
+          <img src={smallImage} className="img-responsive h-[400px] w-[400px]" alt={title} />{" "}
         </a>{" "}
       </div>
     </div>
