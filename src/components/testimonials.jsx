5245b4f44d80f1df7388c793ba3fef5c52a4d5ed
dnl-kg <dnlgakpetor@gmail.com>
2024-04-05 15:18:21 -0400

    updated Sermons page

--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -5,22 +5,25 @@ export const Testimonials = (props) => {
     <div id="testimonials">
       <div className="container">
         <div className="section-title text-center">
-          <h2>What our clients say</h2>
+          <h2>Messages</h2>
         </div>
         <div className="row">
           {props.data
             ? props.data.map((d, i) => (
                 <div key={`${d.name}-${i}`} className="col-md-4">
-                  <div className="testimonial">
-                    <div className="testimonial-image">
-                      {" "}
-                      <img src={d.img} alt="" />{" "}
+                  <a href={d.link}>
+                    <div className="testimonial">
+                      <div className="testimonial-image">
+                        {" "}
+                        <img src={d.img} alt="" />{" "}
+                      </div>
+                      <div className="testimonial-content">
+                        <p className="font-bold">{d.title}</p>
+                        <p>"{d.text}"</p>
+                        <div className="testimonial-meta"> - {d.name} </div>
+                      </div>
                     </div>
-                    <div className="testimonial-content">
-                      <p>"{d.text}"</p>
-                      <div className="testimonial-meta"> - {d.name} </div>
-                    </div>
-                  </div>
+                  </a>
                 </div>
               ))
             : "loading"}
